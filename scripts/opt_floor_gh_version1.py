5ab59d2b742a3b28dda5e11a19f25f0c838fceba
Jeremy <519726058@qq.com>
2023-04-10 23:47:41 +0800

    0410

---
+++ scripts/opt_floor_gh_version1.py
@@ -0,0 +1,32 @@
+from topo_opt import TopoOpt
+import taichi as tc
+
+# Initialize
+version = 0
+n = 60
+volume_fraction = 0.5
+narrow_band = True
+
+opt = TopoOpt(res=(n,n,n),version=version,volume_fraction=volume_fraction,grid_update_start=5 if narrow_band else 1000000,fix_cells_near_force=False,fix_cells_at_dirichlet=False,progressive_vol_frac=0,connectivity_filtering=True,fixed_cell_density=1)
+
+
+tex = tc.Texture('mesh',translate=(0.5,0.5,0.5),scale=(1, 1, 1),adaptive=False,filename='projects/spgrid/data/floor_test2.obj')
+
+
+opt.populate_grid(domain_type='texture', tex_id=tex.id)
+
+opt.general_action(action='add_box_dirichlet_bc', axis_to_fix='xyz', bound0=(0.43, -0.5, -0.5), bound1=(0.5, -0.43, 0.5))
+
+opt.general_action(action='add_box_dirichlet_bc', axis_to_fix='xyz', bound0=(0.43, 0.43, -0.5), bound1=(0.5, 0.5, 0.5))
+
+opt.general_action(action='add_box_dirichlet_bc', axis_to_fix='xyz', bound0=(-0.5, 0.43, -0.5), bound1=(-0.43, 0.5, 0.5))
+
+opt.general_action(action='add_box_dirichlet_bc', axis_to_fix='xyz', bound0=(-0.5, -0.5, -0.5), bound1=(-0.43, -0.43, 0.5))
+
+opt.add_plane_load(force=(0.0, 0.0, -10000.0),axis=2,extreme=-1,bound1=(-0.41, -0.19, -0.5),bound2=(0.08, 0.41, 0.5))
+
+
+
+# Optimize
+opt.run()
+
